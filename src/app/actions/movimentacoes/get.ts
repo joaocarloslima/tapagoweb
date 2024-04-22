8119a7b8a90f00b05dd07bbd5543f121468a7200
joaocarloslima <joaocarloslima@me.com>
2024-04-22 18:51:42 -0300

    enable filter fields

--- src/app/actions/movimentacoes/get.ts
+++ src/app/actions/movimentacoes/get.ts
@@ -1,8 +1,12 @@
 'use server'
 
-export async function get() {
-    await new Promise(r => setTimeout(r, 1000))
-    const resp = await fetch(process.env.API_BASE_URL + "/movimentacao", { next: { revalidate: 0 } })
+export async function get(categoria?: any, mes?: number) {
+    const queryParam = new URLSearchParams()
+    if (categoria) queryParam.append('categoria', categoria)
+    if (mes) queryParam.append('mes', mes.toString())
+    
+    const url = `${process.env.API_BASE_URL}/movimentacao?${queryParam.toString()}`
+    const resp = await fetch(`${process.env.API_BASE_URL}/movimentacao?${queryParam.toString()}`, { next: { revalidate: 0 } })
     const json = await resp.json()
     return json.content
 }
--- src/app/movimentacoes/page.tsx
+++ src/app/movimentacoes/page.tsx
@@ -1,12 +1,7 @@
-import { CategoriaSelect } from "@/components/CategoriaSelect";
-import { MesesSelect } from "@/components/MesesSelect";
-import NavBar from "@/components/NavBar";
-import { Button, Pagination } from "@nextui-org/react";
-import { Filter, Plus } from "lucide-react";
-import Link from "next/link";
 import { getCategorias } from "../actions/categorias/get";
+import NavBar from "@/components/NavBar";
 import { get } from "../actions/movimentacoes/get";
-import { MovimentacaoItem } from "./MovimentacaoItem";
+import { MovimentacaoData } from "./MovimentacaoData";
 
 export default async function Movimentacoes() {
   const movimentacoes: Array<Movimentacao> = await get()
@@ -16,30 +11,12 @@ export default async function Movimentacoes() {
     <main className="flex min-h-screen flex-col items-center">
       <NavBar active="movimentacoes" />
 
-      <section className="flex flex-col gap-4 bg-slate-800 w-4/5 max-w-[1024px] mt-4 p-6 rounded">
-        <div className="flex justify-between">
-          <h2 className="text-2xl font-bold">Movimentações</h2>
-          <Link href="/movimentacoes/new">
-            <Button color="primary" startContent={<Plus />}>nova movimentação</Button>
-          </Link>
-        </div>
-
-        <div className="flex gap-4 items-center border-1 border-white/10 p-4 rounded">
-          <Filter size={44} />
-          <CategoriaSelect categorias={categorias} />
-          <MesesSelect />
-        </div>
-
-        {movimentacoes.map(movimentacao =>
-          <MovimentacaoItem movimentacao={movimentacao} />
-        )}
-
-      </section>
-
-      <Pagination isCompact showControls total={10} initialPage={1} className="mt-6" />
+      <MovimentacaoData 
+        movimentacoes={movimentacoes} 
+        categorias={categorias}
+       />
 
     </main>
-
 
   );
 }
